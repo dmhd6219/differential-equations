80ebfc8e80014d7936bd43374db0aba872001f72
dmhd6210 <svyatoslavsvyatkin@yandex.ru>
2023-12-11 03:38:22 +0300

    add euler function

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -5,20 +5,13 @@
 
 using namespace std;
 
-double max(vector<double> vec) {
-    double mx = vec[0];
-    for (double val: vec) {
-        mx = max(mx, val);
-    }
-    return mx;
+
+double f_x(double x, double y, double t) {
+    return y;
 }
 
-double f(double x) {
-    return cos(6 * x);
-}
-
-double fs(double x) {
-    return sin(6 * x) / 6;
+double f_y(double x, double y, double t) {
+    return -4 * x;
 }
 
 void print_vector(const string &name, vector<double> &vec) {
@@ -37,166 +30,54 @@ void print_vector(const string &name, const vector<int> &vec) {
     cout << endl;
 }
 
-void exact_solution(double y0, double a, double b, int n, vector<double> &xi, vector<double> &yi) {
-    xi.push_back(a);
+void euler_method(double a, double b, double x0, double y0, int n,
+                  vector<double> &xi, vector<double> &yi, vector<double> &ti) {
+    double h = (b - a) / (n - 1);
+
+    xi.push_back(x0);
     yi.push_back(y0);
 
-    double h = (b - a) / (n - 1);
-    for (int i = 0; i < n - 1; i++) {
-        xi.push_back(xi[i] + h);
-        yi.push_back(fs(xi[i + 1]));
+    for (int i = 0; i < n - 1; ++i) {
+        ti.push_back(ti[i] + h);
+        xi.push_back(xi[i] + h * f_x(xi[i], yi[i], ti[i]));
+        yi.push_back(yi[i] + h * f_y(xi[i], yi[i], ti[i]));
     }
-}
-
-void euler(double y0, double a, double b, int n, vector<double> &xi, vector<double> &yi) {
-    xi.push_back(a);
-    yi.push_back(y0);
-
-    double h = (b - a) / (n - 1);
-    for (int i = 0; i < n - 1; i++) {
-        xi.push_back(xi[i] + h);
-        yi.push_back(yi[i] + h * f(xi[i]));
-    }
-}
-
-void improved_euler(double y0, double a, double b, int n, vector<double> &xi, vector<double> &yi) {
-    xi.push_back(a);
-    yi.push_back(y0);
-
-    double h = (b - a) / (n - 1);
-    for (int i = 0; i < n - 1; i++) {
-        xi.push_back(xi[i] + h);
-
-        double k1i = f(xi[i]);
-        double k2i = f(xi[i] + h);
-        yi.push_back(yi[i] + (h / 2) * (k1i + k2i));
-    }
-}
-
-void runge_kutta(double y0, double a, double b, int n, vector<double> &xi, vector<double> &yi) {
-    xi.push_back(a);
-    yi.push_back(y0);
-
-    double h = (b - a) / (n - 1);
-    for (int i = 0; i < n - 1; i++) {
-        xi.push_back(xi[i] + h);
-
-        double k1 = f(xi[i]);
-        double k2 = f(xi[i] + h / 2);
-        double k3 = f(xi[i] + h / 2);
-        double k4 = f(xi[i] + h);
-        yi.push_back(yi[i] + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4));
-    }
-}
-
-void local_errors(vector<double> xi, vector<double> yi, vector<double> &ei) {
-    for (int i = 0; i < xi.size(); i++) {
-        ei.push_back(abs(fs(xi[i]) - yi[i]));
-    }
-}
-
-void global_errors(void (*function)(double, double, double, int, vector<double> &, vector<double> &),
-                   vector<double> &ge, const vector<int> &ni, double y0, double a, double b) {
-
-    for (int i: ni) {
-        vector<double> xi, yi;
-        function(y0, a, b, i, xi, yi);
-        vector<double> ei;
-        local_errors(xi, yi, ei);
-        ge.push_back(max(ei));
-    }
-}
-
-void plot_graph(std::vector<double> x, std::vector<double> y) {
-    FILE *gnuplot = popen("gnuplot -persist", "w");
-    fprintf(gnuplot, "plot '-' with lines\n");
-    for (int i = 0; i < x.size(); i++) {
-        fprintf(gnuplot, "%f %f\n", x[i], y[i]);
-    }
-    fprintf(gnuplot, "e\n");
-    pclose(gnuplot);
 }
 
 
 int main() {
-    double y0 = 0;
+    double x0 = 1;
+    double y0 = 2;
+
     double a = 0;
-    double b = 1;
+    double b = M_PI;
 
-    int n, n1, n2, task;
-    cin >> n >> n1 >> n2 >> task;
+    int n, task;
+    cin >> n >> task;
 
-    vector<double> xi, yi, ei, ge;
-    vector<int> ni;
-    for (int i = n1; i <= n2; i++) {
-        ni.push_back(i);
-    }
+    vector<double> xi, yi, ti;
 
     switch (task) {
         case 1:
-            exact_solution(y0, a, b, n, xi, yi);
+            euler_method(a, b, x0, y0, n, xi, yi, ti);
 
-            print_vector("xi=", xi);
-            print_vector("y(xi)=", yi);
-            break;
-        case 2:
-            euler(y0, a, b, n, xi, yi);
-
-            print_vector("xi=", xi);
+            print_vector("ti=", ti);
+            print_vector("Euler_xi=", xi);
             print_vector("Euler_yi=", yi);
             break;
+        case 2:
+//            euler(y0, a, b, n, xi, yi);
+//
+//            print_vector("xi=", xi);
+//            print_vector("Euler_yi=", yi);
+//            break;
         case 3:
-            improved_euler(y0, a, b, n, xi, yi);
+//            improved_euler(y0, a, b, n, xi, yi);
+//
+//            print_vector("xi=", xi);
+//            print_vector("iEuler_yi=", yi);
+//            break;
 
-            print_vector("xi=", xi);
-            print_vector("iEuler_yi=", yi);
-            break;
-        case 4:
-            runge_kutta(y0, a, b, n, xi, yi);
-
-            print_vector("xi=", xi);
-            print_vector("RK4_yi=", yi);
-            break;
-        case 5:
-            euler(y0, a, b, n, xi, yi);
-            local_errors(xi, yi, ei);
-
-            print_vector("xi=", xi);
-            print_vector("Euler_LE(xi)=", ei);
-            break;
-        case 6:
-            improved_euler(y0, a, b, n, xi, yi);
-            local_errors(xi, yi, ei);
-
-            print_vector("xi=", xi);
-            print_vector("iEuler_LE(xi)=", ei);
-            break;
-        case 7:
-            runge_kutta(y0, a, b, n, xi, yi);
-            local_errors(xi, yi, ei);
-
-            print_vector("xi=", xi);
-            print_vector("RK4_LE(xi)=", ei);
-            break;
-        case 8:
-            global_errors(euler, ge, ni, y0, a, b);
-
-            print_vector("ni=", ni);
-            print_vector("Euler_GE(n)=", ge);
-            break;
-        case 9:
-            global_errors(improved_euler, ge, ni, y0, a, b);
-
-
-            print_vector("ni=", ni);
-            print_vector("iEuler_GE(n)=", ge);
-            break;
-        case 10:
-            global_errors(runge_kutta, ge, ni, y0, a, b);
-
-            print_vector("ni=", ni);
-            print_vector("RK4_GE(n)=", ge);
-            break;
         default:
             cout << "No such task as " << task << "!!!" << endl;
     }
