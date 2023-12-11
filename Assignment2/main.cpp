d04213f41b497b005fe52c334103707b74e843d1
dmhd6210 <svyatoslavsvyatkin@yandex.ru>
2023-12-11 03:40:10 +0300

    add improved euler function

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -30,7 +30,7 @@ void print_vector(const string &name, const vector<int> &vec) {
     cout << endl;
 }
 
-void euler_method(double a, double b, double x0, double y0, int n,
+void euler(double a, double b, double x0, double y0, int n,
                   vector<double> &xi, vector<double> &yi, vector<double> &ti) {
     double h = (b - a) / (n - 1);
 
@@ -41,6 +41,26 @@ void euler_method(double a, double b, double x0, double y0, int n,
         ti.push_back(ti[i] + h);
         xi.push_back(xi[i] + h * f_x(xi[i], yi[i], ti[i]));
         yi.push_back(yi[i] + h * f_y(xi[i], yi[i], ti[i]));
+    }
+}
+
+void improved_euler(double a, double b, double x0, double y0, int n,
+                           vector<double> &xi, vector<double> &yi, vector<double> &ti) {
+    xi.push_back(x0);
+    yi.push_back(y0);
+    double h = (b - a) / (n - 1);
+
+    for (int i = 0; i < n - 1; ++i) {
+        ti.push_back(ti[i] + h);
+
+        double k1x = f_x(xi[i], yi[i], ti[i]);
+        double k1y = f_y(xi[i], yi[i], ti[i]);
+
+        double k2x = f_x(xi[i] + h * k1x, yi[i] + h * k1y, ti[i + 1]);
+        double k2y = f_y(xi[i] + h * k1x, yi[i] + h * k1y, ti[i + 1]);
+
+        xi.push_back(xi[i] + h / 2 * (k1x + k2x));
+        yi.push_back(yi[i] + h / 2 * (k1y + k2y));
     }
 }
 
@@ -59,18 +79,18 @@ int main() {
 
     switch (task) {
         case 1:
-            euler_method(a, b, x0, y0, n, xi, yi, ti);
+            euler(a, b, x0, y0, n, xi, yi, ti);
 
             print_vector("ti=", ti);
             print_vector("Euler_xi=", xi);
             print_vector("Euler_yi=", yi);
             break;
         case 2:
-//            euler(y0, a, b, n, xi, yi);
-//
-//            print_vector("xi=", xi);
-//            print_vector("Euler_yi=", yi);
-//            break;
+            improved_euler(a, b, x0, y0, n, xi, yi, ti);
+
+            print_vector("ti=", ti);
+            print_vector("iEuler_xi=", xi);
+            print_vector("iEuler_yi=", yi);
         case 3:
 //            improved_euler(y0, a, b, n, xi, yi);
 //
