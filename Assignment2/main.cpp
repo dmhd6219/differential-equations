ea5d962e0c6073dfb35c2ff143a52911e929ab73
dmhd6210 <svyatoslavsvyatkin@yandex.ru>
2023-12-11 03:42:54 +0300

    add runge kutta function

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -53,14 +53,39 @@ void improved_euler(double a, double b, double x0, double y0, int n,
     for (int i = 0; i < n - 1; ++i) {
         ti.push_back(ti[i] + h);
 
-        double k1x = f_x(xi[i], yi[i], ti[i]);
-        double k1y = f_y(xi[i], yi[i], ti[i]);
+        double k1_x = f_x(xi[i], yi[i], ti[i]);
+        double k1_y = f_y(xi[i], yi[i], ti[i]);
 
-        double k2x = f_x(xi[i] + h * k1x, yi[i] + h * k1y, ti[i + 1]);
-        double k2y = f_y(xi[i] + h * k1x, yi[i] + h * k1y, ti[i + 1]);
+        double k2_x = f_x(xi[i] + h * k1_x, yi[i] + h * k1_y, ti[i + 1]);
+        double k2_y = f_y(xi[i] + h * k1_x, yi[i] + h * k1_y, ti[i + 1]);
 
-        xi.push_back(xi[i] + h / 2 * (k1x + k2x));
-        yi.push_back(yi[i] + h / 2 * (k1y + k2y));
+        xi.push_back(xi[i] + h / 2 * (k1_x + k2_x));
+        yi.push_back(yi[i] + h / 2 * (k1_y + k2_y));
+    }
+}
+
+void runge_kutta(double a, double b, double x0, double y0, int n, vector<double> &xi, vector<double> &yi, vector<double>& ti) {
+    xi.push_back(x0);
+    yi.push_back(y0);
+    double h = (b-a)/(n-1);
+
+    for (int i = 0; i < n - 1; ++i) {
+        ti.push_back(ti[i] + h);
+
+        double k1_x = f_x(xi[i], yi[i], ti[i]);
+        double k1_y = f_y(xi[i], yi[i], ti[i]);
+
+        double k2_x = f_x(xi[i] + h / 2 * k1_x, yi[i] + h / 2 * k1_y, ti[i] + h / 2);
+        double k2_y = f_y(xi[i] + h / 2 * k1_x, yi[i] + h / 2 * k1_y, ti[i] + h / 2);
+
+        double k3_x = f_x(xi[i] + h / 2 * k2_x, yi[i] + h / 2 * k2_y, ti[i] + h / 2);
+        double k3_y = f_y(xi[i] + h / 2 * k2_x, yi[i] + h / 2 * k2_y, ti[i] + h / 2);
+
+        double k4_x = f_x(xi[i] + h * k3_x, yi[i] + h * k3_y, ti[i] + h);
+        double k4_y = f_y(xi[i] + h * k3_x, yi[i] + h * k3_y, ti[i] + h);
+
+        xi.push_back(xi[i] + h/6 * (k1_x + 2 * k2_x + 2 * k3_x + k4_x));
+        yi.push_back(yi[i] + h/6 * (k1_y + 2 * k2_y + 2 * k3_y + k4_y));
     }
 }
 
@@ -92,11 +117,11 @@ int main() {
             print_vector("iEuler_xi=", xi);
             print_vector("iEuler_yi=", yi);
         case 3:
-//            improved_euler(y0, a, b, n, xi, yi);
-//
-//            print_vector("xi=", xi);
-//            print_vector("iEuler_yi=", yi);
-//            break;
+            runge_kutta(a, b, x0, y0, n, xi, yi, ti);
+
+            print_vector("ti=", ti);
+            print_vector("RK4_xi=", xi);
+            print_vector("RK4_yi=", yi);
 
         default:
             cout << "No such task as " << task << "!!!" << endl;
